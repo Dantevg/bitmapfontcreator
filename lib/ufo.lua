ca9508d3b7db01a1c35a656c5a9afc0a70124dfb
Dantevg <mail@dantevg.nl>
2020-04-26 18:00:47 +0200

    Add xml2lua, my ufo lib

---
+++ lib/ufo.lua
@@ -0,0 +1,92 @@
+--[[
+	
+	UFO Unified Font Object XML file helper
+	by Dante van Gemert
+	
+	This lib helps generating the XML
+	for the files needed for a UFO
+	
+]]--
+
+local ufo = {}
+
+ufo.plistHeader = [[
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN"
+"http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+]]
+
+local function tag( name, attr )
+	local s = "<"..name
+	for k, v in pairs( attr or {} ) do
+		s = s.." "..tostring(k)..'="'..tostring(v)..'"'
+	end
+	return s..">"
+end
+
+function ufo.toXML( data, level )
+	if type(data) ~= "table" then error("Expected table") end
+	level = level or 0
+	if level > 128 then error( "Max table depth (128) reached (recursive tables are not supported)" ) end
+	
+	local s = tag( data.name, data.attr )
+	
+	if #data == 1 and type(data[1]) ~= "table" then
+		s = s..tostring( data[1] )
+	else
+		for _, v in ipairs(data) do
+			if type(v) == "table" then
+				s = s.."\n"..string.rep( "    ", level+1 )..ufo.toXML( v, level+1 )
+			else
+				s = s.."\n"..string.rep( "    ", level+1 )..tostring( data[i] )
+			end
+		end
+		s = s.."\n"..string.rep( "    ", level )
+	end
+	
+	return s.."</"..data.name..">"
+end
+
+local xmlTypes = {
+	string = "string",
+	number = "integer",
+}
+
+function ufo.array( input, type )
+	if type(input) ~= "table" then error("Expected table") end
+	
+	local data = {name = "array"}
+	
+	for _, v in ipairs(input) do
+		table.insert( data, {name = type, v} )
+	end
+	
+	return data
+end
+
+function ufo.dict(input)
+	if type(input) ~= "table" then error("Expected table") end
+	
+	local data = {name = "dict"}
+	
+	for k, v in pairs(input) do
+		table.insert( data, {name = "key", tostring(k)} )
+		table.insert( data, {name = xmlTypes[type(v)], v} )
+	end
+	
+	return data
+end
+
+function ufo.plist(input)
+	if type(input) ~= "table" then error("Expected table") end
+	
+	data = {
+		name = "plist",
+		attr = {version = "1.0"},
+		input,
+	}
+	
+	return data
+end
+
+return ufo
--- main.lua
+++ main.lua
@@ -5,12 +5,25 @@
 	
 	main file
 	
+	Project resources:
+	- http://unifiedfontobject.org/versions/ufo3/
+	- https://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=IWS-Chapter08
+	- https://notabug.org/pgimeno/Gspot/wiki
+	
 ]]--
 
 function love.load()
+	xml = require "lib/xml2lua"
+	toXML = require "lib/xml"
+	ufo = require "lib/ufo"
+	font = require "font"
 	gui = require("lib/Gspot"):setComponentMax(255)
 	
 	canvas = love.graphics.newCanvas()
+	
+	local fnt = font({name = "My Font!"})
+	fnt.author = "Me!"
+	print( fnt:generateXML("metainfo") )
 end
 
 function love.update(dt)

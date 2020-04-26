83f8254a04bd55131cbc77081d56f1940f2038d8
Dantevg <mail@dantevg.nl>
2020-04-26 18:00:53 +0200

    Add font lib

---
+++ font.lua
@@ -0,0 +1,64 @@
+--[[
+	
+	Font handling lib
+	by Dante van Gemert
+	
+]]--
+
+local font = {}
+
+function font.new(options)
+	if not options then error("Expected options") end
+	return setmetatable( {
+		name = options.name or "",
+		author = options.author or "",
+		glyphs = {},
+	}, {__index = font} )
+end
+
+
+
+-- XML FILE OUTPUT
+-- Generates output files as described in:
+-- http://unifiedfontobject.org/versions/ufo3/
+
+font.outputfiles = {}
+
+-- http://unifiedfontobject.org/versions/ufo3/metainfo.plist/
+function font.outputfiles.metainfo(font)
+	if type(font.author) ~= "string" or #font.author < 1 then
+		return false, "Author name must be present"
+	end
+	
+	return ufo.plistHeader.."\n"..ufo.toXML(
+		ufo.plist(
+			ufo.dict{
+				creator = font.author,
+				formatVersion = 3,
+			}
+		)
+	)
+end
+
+-- http://unifiedfontobject.org/versions/ufo3/layercontents.plist/
+function font.outputfiles.layercontents(font)
+	
+end
+
+function font:generateXML(what)
+	if not font.outputfiles[what] then
+		error("No such output type")
+	end
+	
+	return font.outputfiles[what](self)
+end
+
+
+
+-- RETURN
+return setmetatable( font, {
+	__index = font,
+	__call = function( _, ... )
+		return font.new(...)
+	end
+} )

cb42462b23efeff71a9a275630cdcde775be36dd
Dantevg <mail@dantevg.nl>
2020-04-26 13:11:14 +0200

    Add Gspot GUI lib

--- main.lua
+++ main.lua
@@ -7,14 +7,34 @@
 	
 ]]--
 
-function love.init()
+function love.load()
+	gui = require("lib/Gspot"):setComponentMax(255)
 	
+	canvas = love.graphics.newCanvas()
 end
 
-function love.update()
-	
+function love.update(dt)
+	gui:update(dt)
 end
 
 function love.draw()
-	
+	gui:draw()
+	love.graphics.draw(canvas)
+end
+
+-- Forward love2d events to Gspot GUI
+function love.keypressed(key)
+	gui:keypress(key)
+end
+function love.textinput(key)
+	gui:textinput(key)
+end
+function love.mousepressed( x, y, btn )
+	gui:mousepress( x, y, btn )
+end
+function love.mousereleased( x, y, btn )
+	gui:mouserelease( x, y, btn )
+end
+function love.wheelmoved( x, y )
+	gui:mousewheel( x, y )
 end

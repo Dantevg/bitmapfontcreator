f498e2a56c84cc58663e0c02be7fa589e932c667
Dantevg <mail@dantevg.nl>
2020-05-07 19:43:58 +0200

    Separate xml header from plist header

--- lib/ufo.lua
+++ lib/ufo.lua
@@ -10,8 +10,11 @@
 
 local ufo = {}
 
-ufo.plistHeader = [[
+ufo.xmlHeader = [[
 <?xml version="1.0" encoding="UTF-8"?>
+]]
+
+ufo.plistHeader = ufo.xmlHeader..[[
 <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN"
 "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 ]]

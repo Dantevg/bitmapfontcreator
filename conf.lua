927fe71bd0d176f47fec4e7b2c7587e565dd681c
Dantevg <mail@dantevg.nl>
2020-06-12 15:46:41 +0200

    Disable unused modules for faster loading

--- conf.lua
+++ conf.lua
@@ -3,4 +3,23 @@ function love.conf(t)
 	t.window.title = "Bitmapfontcreator - loading..."
 	-- t.console = true
 	t.window.resizable = true
+	
+	t.modules.audio = false
+	t.modules.data = false
+	-- t.modules.event = false
+	-- t.modules.font = false
+	-- t.modules.graphics = false
+	-- t.modules.image = false
+	t.modules.joystick = false
+	-- t.modules.keyboard = false
+	t.modules.math = false
+	-- t.modules.mouse = false
+	t.modules.physics = false
+	t.modules.sound = false
+	t.modules.system = false
+	t.modules.thread = false
+	-- t.modules.timer = false
+	t.modules.touch = false
+	t.modules.video = false
+	-- t.modules.window = false
 end

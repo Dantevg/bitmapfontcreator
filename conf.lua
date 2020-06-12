210f87ebea20512f37606cf62f02c58a0f710b01
RedPolygon <mail@dantevg.nl>
2020-06-12 13:54:45 +0200

    Disable console by default

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
 	t.identity  = "bitmapfontcreator"
 	t.window.title = "Bitmapfontcreator - loading..."
-	t.console = true
+	-- t.console = true
 	t.window.resizable = true
 end
